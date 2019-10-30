b1f568b8a53f00988c777c97c0392494a5fd53f6
Kent Chen <chkche1@gmail.com>
2019-10-30 01:01:01 -0700

    Save on one variable for Two Fer

--- two-fer/two_fer.go
+++ two-fer/two_fer.go
@@ -4,9 +4,8 @@ import "fmt"
 
 // ShareWith returns "One for you, one for me." when the input string is empty. Otherwise it returns a string referencing the input name
 func ShareWith(name string) string {
-	it := name
-	if len(name) == 0 {
-		it = "you"
+	if name == "" {
+		name = "you"
 	}
-	return fmt.Sprintf("One for %s, one for me.", it)
+	return fmt.Sprintf("One for %s, one for me.", name)
 }
