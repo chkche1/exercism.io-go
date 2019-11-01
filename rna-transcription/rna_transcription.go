26ea9965be97a0bea7bda842c691e5326111bb5a
Kent Chen <chkche1@gmail.com>
2019-11-01 00:25:41 -0700

    RNA Transcription

---
+++ rna-transcription/rna_transcription.go
@@ -0,0 +1,16 @@
+package strand
+
+var rnaMap = map[rune]rune {
+	'G': 'C',
+	'C': 'G',
+	'T': 'A',
+	'A': 'U',
+}
+
+func ToRNA(dna string) string {
+	res := make([]rune, len(dna))
+	for i, c := range dna {
+		res[i] = rnaMap[c]
+	}
+	return string(res)
+}
