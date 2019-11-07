9cc86dc7460d1ff9bf921b7648477e5595bcdda5
Kent Chen <chkche1@gmail.com>
2019-11-06 22:56:01 -0800

    Leap

---
+++ leap/leap.go
@@ -0,0 +1,16 @@
+// A package that holds a method that determines whether a year is a leap year
+package leap
+
+// Given a year, report if it is a leap year.
+func IsLeapYear(n int) bool {
+	if n % 4 != 0 {
+		return false
+	}
+
+	if n % 400 == 0 {
+		return true
+	} else if n % 100 == 0 {
+		return false
+	}
+	return true
+}
