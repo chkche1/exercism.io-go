2afc05635a10b576a4b2b80205081450411edb5e
Kent Chen <chkche1@gmail.com>
2019-11-20 13:34:21 -0800

    Change Scrabble Score from map-based to switch-based

--- scrabble-score/scrabble_score.go
+++ scrabble-score/scrabble_score.go
@@ -3,48 +3,27 @@ package scrabble
 import "unicode"
 
 /*
-Letter                           Value
-A, E, I, O, U, L, N, R, S, T       1
-D, G                               2
-B, C, M, P                         3
-F, H, V, W, Y                      4
-K                                  5
-J, X                               8
-Q, Z                               10
+Given a word, compute the Scrabble score for that word.
 */
-var points = map[rune]int{
-	'A': 1,
-	'E': 1,
-	'I': 1,
-	'O': 1,
-	'U': 1,
-	'L': 1,
-	'N': 1,
-	'R': 1,
-	'S': 1,
-	'T': 1,
-	'D': 2,
-	'G': 2,
-	'B': 3,
-	'C': 3,
-	'M': 3,
-	'P': 3,
-	'F': 4,
-	'H': 4,
-	'V': 4,
-	'W': 4,
-	'Y': 4,
-	'K': 5,
-	'J': 8,
-	'X': 8,
-	'Q': 10,
-	'Z': 10,
-}
-
 func Score(s string) int {
 	res := 0
 	for _, c := range s {
-		res += points[unicode.ToUpper(c)]
+		switch unicode.ToUpper(c) {
+		case 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T':
+			res += 1
+		case 'D', 'G':
+			res += 2
+		case 'B', 'C', 'M', 'P':
+			res += 3
+		case 'F', 'H', 'V', 'W', 'Y':
+			res += 4
+		case 'K':
+			res += 5
+		case 'J', 'X':
+			res += 8
+		case 'Q', 'Z':
+			res += 10
+		}
 	}
 	return res
 }
