224dbfb608815699e8311f88099fa15990f322f3
Kent Chen <chkche1@gmail.com>
2019-11-23 19:46:05 -0800

    Reverse String

---
+++ reverse-string/reverse_string.go
@@ -0,0 +1,13 @@
+package reverse
+
+func Reverse(s string) string {
+	if len(s) == 0 {
+		return s
+	}
+
+	res := []rune(s)
+	for l, r := 0, len(res) - 1; l < r; l, r = l + 1, r - 1 {
+		res[l], res[r] = res[r], res[l]
+	}
+	return string(res)
+}
